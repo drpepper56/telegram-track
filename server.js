d6f5cf7b1068f15f14df2f3e91f58449502ff4f3
Karol Bura <virtualreader76@gmail.com>
2025-03-18 13:23:41 +0000

    config fix2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("telegram_front")); // Serves static files from "public" folder
+app.use(express.static("public")); // Serves static files from "public" folder
 // could go to shit
 
 app.listen(port, () => {
