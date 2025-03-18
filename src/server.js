c806e2197ee8ce20dd750418a8df2907f0814c75
Karol Bura <virtualreader76@gmail.com>
2025-03-18 15:51:46 +0000

    deploy to heroku

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public")); // Serves static files from "public" folder
 // could go to shit
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
