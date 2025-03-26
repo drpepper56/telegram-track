764fbfb77f69b46563eba62e27af494aacab17a3
Karol Bura <virtualreader76@gmail.com>
2025-03-26 21:56:36 +0000

    quick heroku deploy

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ function notification_handler() {
     const queryString = window.location.search;
     const params = new URLSearchParams(queryString);
 
-    tg.showAlert("script kinda works")
+    tg.showAlert(params.toString());
     
     if (params.has('startapp')) {
         const startAppParam = params.get('startapp');
