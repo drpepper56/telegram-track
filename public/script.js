e5e34e1c9597aad6eab7950c2f5b7ffc1c20e1c7
Karol Bura <virtualreader76@gmail.com>
2025-04-03 21:38:06 +0100

    quick heroku deploy

--- public/script.js
+++ public/script.js
@@ -8,7 +8,8 @@ const crypto = require('crypto');
     CONSTANTS
 */
 
-const BACKEND_LINK = 'https://webhook.lemoncardboard.uk';
+// const BACKEND_LINK = 'https://webhook.lemoncardboard.uk';
+const BACKEND_LINK = 'https://127.0.0.1:8080';
 
 /*
     Init TWA
