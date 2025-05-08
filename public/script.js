c8b9e07cba7edf2edbea3405ad99949a381921fb
Karol Bura <virtualreader76@gmail.com>
2025-05-08 18:43:06 +0100

    quick heroku deploy

--- public/script.js
+++ public/script.js
@@ -327,7 +327,7 @@ async function get_tracking_data(tracking_number) {
             } else if (prime_response.ok) {
                 // write successful
                 console.log(success_mes)
-                let response = await second_prime_response.json();
+                let response = await prime_response.json();
                 console.log(response);
                 notify(response);
                 console.log(response.status, " ", response.text());
