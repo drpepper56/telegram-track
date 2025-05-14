0993853323c18158609826e573185fb2ea7c404e
Karol Bura <virtualreader76@gmail.com>
2025-05-14 01:08:46 +0100

    i love working with branches, so cool

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 
-app.use(express.static(path.join(__dirname, './', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public', 'index.html'));
